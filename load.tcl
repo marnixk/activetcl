7c91b83d7b21e280b8e8a5bdf137b317418e07d8
Marnix <m.cook@auckland.ac.nz>
2014-03-13 15:00:25 +1300

    turn it into a package

---
+++ load.tcl
@@ -0,0 +1,10 @@
+package provide activetcl 1.0
+
+set pkg_dir [file dirname [info script]]
+
+source "$pkg_dir/misc.tcl"
+source "$pkg_dir/dbfunctions.tcl"
+source "$pkg_dir/dbpersist.tcl"
+source "$pkg_dir/dbqueries.tcl"
+source "$pkg_dir/dbassocs.tcl"
+
---
+++ pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded activetcl 1.0 [list source [file join $dir "load.tcl"]]
